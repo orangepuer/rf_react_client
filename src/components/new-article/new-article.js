8908f94814e2ff93b5a6e60ff833e5a816ef2ff9
orangepuer <rkhpuer@gmail.com>
2020-05-22 18:57:58 +0300

    Add NewArticle component

---
+++ src/components/new-article/new-article.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import "./new-article.css";
+
+class NewArticle extends Component {
+  render() {
+    return <div></div>
+  }
+}
+
+export default NewArticle;
